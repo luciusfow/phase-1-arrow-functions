10631bf77998b1512479d812df853cfe2ca90e65
Lucius Fowler <luciusfow@gmail.com>
2021-04-15 16:14:14 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+
+    // const divide = function(num1, num2) {
+    //     return num1 / num2
+    // }
+    // const num1 = 2000
+    // const num2 = 100
+    // const divide = function(num1, num2) {
+    //     return num1 / num2
+    // }
+    const divide = (num1, num2) => 2000/ 100 
+        divide()
+    
+    const square = (num) => num * num
+    const add = (num1, num2) => num1+num2
